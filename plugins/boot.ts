5cfd4821f2be7c6028a34b1f68b75676e0d69992
Ivan Bravo Bravo <kolessios@gmail.com>
2021-06-27 06:54:45 -0500

    chore: added settings to the boot.

--- plugins/boot.ts
+++ plugins/boot.ts
@@ -3,12 +3,15 @@ import { Plugin } from '@nuxt/types'
 import ThemeBaseMixin from '@opendreamnet/nuxtjs-base/mixins/BaseMixin'
 import BaseMixin from '../mixins/BaseMixin'
 import { bus } from '../modules/bus'
+import { settings } from '../modules/settings'
 
 Vue.mixin(Vue.extend(ThemeBaseMixin))
 Vue.mixin(BaseMixin)
 
 const plugin: Plugin = async(ctx, inject) => {
   inject('bus', bus)
+
+  inject('settings', Vue.observable(settings))
 }
 
 export default plugin
