1810e2d7f37d979b6b28300317cc81a8db287944
Ivan Bravo Bravo <kolessios@gmail.com>
2021-06-27 06:51:28 -0500

    chore: added file with definitions/constants.

---
+++ modules/defs.ts
@@ -0,0 +1,7 @@
+export const MAX_RECORDS = 100
+
+export const SEED_TEXT = 'banyan-choice-shrewish-oasis-albania'
+
+export const MAX_USERNAME_LENGTH = 20
+
+export const DEFAULT_ENCRYPTION_KEY = 'bR^x&)k4+fx=:3DLYLJ#FI'
