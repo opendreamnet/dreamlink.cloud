5de4b883bde8652a01842a416506a518ee037218
Ivan Bravo Bravo <kolessios@gmail.com>
2021-06-27 06:33:47 -0500

    fix: improved Storage description.

--- components/Dialog/Node.vue
+++ components/Dialog/Node.vue
@@ -2,42 +2,49 @@
   <dialog ref="dialog" class="dialog">
     <Box title="My IPFS Node">
       <div v-if="$ipfs.ready" class="node__cols">
+        <!-- Avatar -->
         <div class="node__left">
           <figure>
             <img :src="avatarURL">
           </figure>
         </div>
 
-        <div class="node__right node__details">
+        <!-- Details -->
+        <div class="prose node__right node__details">
           <p>
             <span class="title">
-              Peer ID: <Tooltip :content="{ content: 'A Peer ID is how each unique IPFS node is identified on the network. The Peer ID is created when the IPFS node is initialized and is essentially a cryptographic hash of the node\'s public key.', appendTo: $el }" />
+              <Tooltip :content="{ content: 'A Peer ID is how each unique IPFS node is identified on the network. The Peer ID is created when the IPFS node is initialized and is essentially a cryptographic hash of the node\'s public key.', appendTo: $el }" />
+              Peer ID:
             </span>
             <span class="value">{{ $ipfs.identity.id }}</span>
           </p>
 
           <p>
             <span class="title">
-              Storage: <Tooltip :content="{ content: 'The total storage space depends on your web browser and does not always mark the correct amount. (It is usually much larger)', appendTo: $el }" />
+              <Tooltip :content="{ content: 'The space occupied by your files. The maximum amount depends on your web browser and the correct amount is not always displayed (it is usually higher).', appendTo: $el }" />
+              Storage:
             </span>
             <NetworkStorageSize class="value" />
           </p>
         </div>
       </div>
 
+      <!-- Error -->
       <div v-else-if="$ipfs.error">
         <p>An error occurred while trying to start the IPFS node, please refresh the page or try another web browser:</p>
-
-        <pre class="code-pre">{{ $ipfs.error }}</pre>
+        <pre class="pre"><code>{{ $ipfs.error }}</code></pre>
       </div>
 
+      <!-- Loading -->
       <div v-else>
         <p>Your node is starting up, wait a minute....</p>
       </div>
 
       <template #footer>
-        <div class="justify-end buttons">
-          <Button v-if="$ipfs.ready" v-tooltip="storageTooltip" class="button--primary button--sm" :loading="gcLoading" @click="freeUp">
+        <div class="flex justify-end gap-3">
+          <Button
+            v-if="$ipfs.ready" v-tooltip="storageTooltip" class="button--primary button--sm"
+            :loading="gcLoading" @click="freeUp">
             Free up storage
           </Button>
 
