f234b29257b05092dee0aa5987d8817372363ba6
Ivan Bravo Bravo <kolessios@gmail.com>
2021-06-28 11:54:35 -0500

    perf: removed "await" at startup

--- plugins/ipfs.ts
+++ plugins/ipfs.ts
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import { Plugin } from '@nuxt/types'
-import { IPFS } from '@opendreamnet/ipfs'
 import { storage } from '../modules/storage'
 import { ipfs } from '../modules/ipfs'
 
 const plugin: Plugin = async(ctx, inject) => {
   inject('ipfs', Vue.observable(ipfs))
 
-  await storage.init()
+  storage.init()
   inject('storage', Vue.observable(storage))
 
   if (process.env.NODE_ENV !== 'production') {
