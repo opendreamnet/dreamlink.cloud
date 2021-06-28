91c9b877c958f8076382a8396edf11a8f0ae785c
Ivan Bravo Bravo <kolessios@gmail.com>
2021-06-28 16:30:29 -0500

    style: improved footer.

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,45 +1,84 @@
 <template>
   <footer class="footer">
-    <nav class="nav">
-      <NuxtLink to="/docs/tos" class="item">
-        Terms of Service
-      </NuxtLink>
+    <div class="container">
+      <div class="footer__copyright">
+        <p>
+          <a href="https://github.com/opendreamnet/dreamlink.cloud" target="_blank" class="version">v{{ $config.version }}</a>
+        </p>
 
-      <NuxtLink to="/docs/privacy" class="item">
-        Privacy Policy
-      </NuxtLink>
-    </nav>
+        <p>
+          © {{ new Date().getFullYear() }} <a href="https://opendreamnet.com" target="_blank">OpenDreamNet</a>. Distributed under the <a href="https://github.com/opendreamnet/dreamlink.cloud/blob/master/LICENSE" target="_blank">GNU AGPLv3 license</a>.
+        </p>
 
-    <div class="footer__copyright">
-      <p>© {{ new Date().getFullYear() }} OpenDreamNet. {{ $config.name }} is distributed under the <a href="https://github.com/opendreamnet/dreamlink.cloud/blob/master/LICENSE" target="_blank">GNU AGPLv3 license</a>.</p>
-      <p><a href="https://github.com/opendreamnet/dreamlink.cloud" target="_blank" class="version">v{{ $config.version }}</a></p>
+        <p>
+          <a href="https://ipfs.io/" target="_blank">IPFS</a> is a trademark of <a href="https://protocol.ai/" target="_blank">Protocol Labs</a>.
+        </p>
+      </div>
+
+      <div class="w-16 h-16" />
+
+      <nav class="nav">
+        <NuxtLink to="/docs/tos" class="item">
+          Terms of Service
+        </NuxtLink>
+
+        <NuxtLink to="/docs/privacy" class="item">
+          Privacy Policy
+        </NuxtLink>
+
+        <a href="mailto:hello@opendreamnet.com" class="item">
+          Contact
+        </a>
+
+        <a href="https://twitter.com/opendreamnetdev" target="_blank" class="item">
+          Twitter
+        </a>
+      </nav>
     </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
 .footer {
-  @apply space-y-3;
+  @apply border-t border-menus p-10;
+  @apply mt-40 #{!important};
+
+  @screen md {
+    @apply text-sm;
+  }
+
+  .container {
+    @apply flex flex-col-reverse justify-between;
+
+    @screen md {
+      @apply flex-row;
+    }
+  }
 }
 
 .nav {
-  @apply flex flex-wrap justify-center items-center w-full space-x-6;
-  @apply px-6;
+  @apply space-y-2;
 
-  @screen md {
-    @apply px-0;
-  }
+  a {
+    @apply block font-semibold text-white;
 
-  a:hover {
-    @apply text-primary underline;
+    &:hover {
+      @apply text-primary underline;
+    }
   }
 }
 
 .footer__copyright {
-  @apply text-sm text-center pb-6;
-
+  p {
+    @apply mb-1;
+  }
+  
   a {
-    @apply underline;
+    @apply text-white;
+
+    &:hover {
+      @apply text-primary underline;
+    }
   }
 }
 
